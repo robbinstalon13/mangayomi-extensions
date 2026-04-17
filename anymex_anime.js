9972abfcac464bb2d62430adeede31bb158892c7
robbinstalon13 <robbinstalon13@gmail.com>
2026-04-17 00:00:01 -0400

    Fix anime streams: switch to 2embed.cc (himer365ery.com expired)

--- anymex_anime.js
+++ anymex_anime.js
@@ -1,12 +1,12 @@
 const mangayomiSources = [{
     "name": "AnymeX Anime",
     "lang": "en",
-    "baseUrl": "https://api.themoviedb.org",
-    "apiUrl": "https://api.themoviedb.org/3",
+    "baseUrl": "https://api.2embed.cc",
+    "apiUrl": "https://api.2embed.cc",
     "iconUrl": "https://raw.githubusercontent.com/RyanYuuki/AnymeX/main/assets/images/logo.png",
     "typeSource": "multi",
     "itemType": 1,
-    "version": "0.0.1",
+    "version": "0.0.2",
     "pkgPath": "anymex_anime.js"
 }];
 
@@ -15,28 +15,16 @@ class DefaultExtension extends MProvider {
     constructor() {
         super();
         this.client = new Client();
+        this.apiBase = "https://api.2embed.cc";
         this.tmdbKey = "ad301b7cc82ffe19273e55e4d4206885";
         this.tmdbBase = "https://api.themoviedb.org/3";
         this.imgBase = "https://image.tmdb.org/t/p/w500";
-        this.streamBase = "https://himer365ery.com";
-        this.streamHost = "https://jarvi366dow.com";
+        this.embedBase = "https://www.2embed.cc";
     }
 
-    getStreamHeaders() {
-        return {
-            "Referer": "https://himer365ery.com/",
-            "Origin": "https://himer365ery.com"
-        };
-    }
-
-    mapItem(e, type) {
-        var poster = e.poster_path || e.backdrop_path;
-        return {
-            name: e.title || e.name || "Unknown",
-            link: JSON.stringify({ tmdbId: e.id, type: type }),
-            imageUrl: poster ? (this.imgBase + poster) : "",
-            description: e.overview || ""
-        };
+    async apiGet(url) {
+        var resp = await this.client.get(url);
+        return JSON.parse(resp.body);
     }
 
     async tmdbGet(path) {
@@ -45,21 +33,23 @@ class DefaultExtension extends MProvider {
         return JSON.parse(resp.body);
     }
 
+    mapTmdbItem(e, type) {
+        return {
+            name: e.name || e.title || "Unknown",
+            link: JSON.stringify({ tmdbId: e.id, type: type }),
+            imageUrl: e.poster_path ? (this.imgBase + e.poster_path) : "",
+            description: e.overview || ""
+        };
+    }
+
     async getPopular(page) {
         try {
             var data = await this.tmdbGet("/discover/tv?sort_by=popularity.desc&with_genres=16&with_original_language=ja&page=" + page);
+            var self = this;
             var list = (data.results || []).map(function(e) {
-                return {
-                    name: e.name || "Unknown",
-                    link: JSON.stringify({ tmdbId: e.id, type: "tv" }),
-                    imageUrl: e.poster_path ? ("https://image.tmdb.org/t/p/w500" + e.poster_path) : "",
-                    description: e.overview || ""
-                };
+                return self.mapTmdbItem(e, "tv");
             });
-            return {
-                list: list,
-                hasNextPage: page < (data.total_pages || 1)
-            };
+            return { list: list, hasNextPage: page < (data.total_pages || 1) };
         } catch (e) {
             console.error("getPopular error: " + e);
             return { list: [], hasNextPage: false };
@@ -73,18 +63,11 @@ class DefaultExtension extends MProvider {
     async getLatestUpdates(page) {
         try {
             var data = await this.tmdbGet("/discover/tv?sort_by=first_air_date.desc&with_genres=16&with_original_language=ja&page=" + page);
+            var self = this;
             var list = (data.results || []).map(function(e) {
-                return {
-                    name: e.name || "Unknown",
-                    link: JSON.stringify({ tmdbId: e.id, type: "tv" }),
-                    imageUrl: e.poster_path ? ("https://image.tmdb.org/t/p/w500" + e.poster_path) : "",
-                    description: e.overview || ""
-                };
+                return self.mapTmdbItem(e, "tv");
             });
-            return {
-                list: list,
-                hasNextPage: page < (data.total_pages || 1)
-            };
+            return { list: list, hasNextPage: page < (data.total_pages || 1) };
         } catch (e) {
             console.error("getLatestUpdates error: " + e);
             return { list: [], hasNextPage: false };
@@ -93,40 +76,26 @@ class DefaultExtension extends MProvider {
 
     async search(query, page, filters) {
         try {
-            var tvData = await this.tmdbGet("/search/tv?query=" + encodeURIComponent(query) + "&page=" + page + "&include_adult=false");
-            var movieData = await this.tmdbGet("/search/movie?query=" + encodeURIComponent(query) + "&page=" + page + "&include_adult=false");
+            var tvUrl = this.apiBase + "/searchtv?q=" + encodeURIComponent(query) + "&page=" + page;
+            var tvData = await this.apiGet(tvUrl);
 
-            var tvResults = (tvData.results || []).filter(function(e) {
-                return e.original_language === "ja";
-            }).map(function(e) {
+            var list = (tvData.results || []).map(function(e) {
                 return {
                     name: e.name || "Unknown",
-                    link: JSON.stringify({ tmdbId: e.id, type: "tv" }),
-                    imageUrl: e.poster_path ? ("https://image.tmdb.org/t/p/w500" + e.poster_path) : "",
+                    link: JSON.stringify({
+                        tmdbId: e.tmdb_id,
+                        imdbId: e.imdb_id,
+                        type: "tv"
+                    }),
+                    imageUrl: e.poster || "",
                     description: e.overview || ""
                 };
             });
 
-            var movieResults = (movieData.results || []).filter(function(e) {
-                return e.original_language === "ja";
-            }).map(function(e) {
-                return {
-                    name: e.title || "Unknown",
-                    link: JSON.stringify({ tmdbId: e.id, type: "movie" }),
-                    imageUrl: e.poster_path ? ("https://image.tmdb.org/t/p/w500" + e.poster_path) : "",
-                    description: e.overview || ""
-                };
-            });
-
-            var maxLen = Math.max(tvResults.length, movieResults.length);
-            var mixed = [];
-            for (var i = 0; i < maxLen; i++) {
-                if (i < tvResults.length) mixed.push(tvResults[i]);
-                if (i < movieResults.length) mixed.push(movieResults[i]);
-            }
-
-            var hasNext = page < Math.max(tvData.total_pages || 1, movieData.total_pages || 1);
-            return { list: mixed, hasNextPage: hasNext };
+            return {
+                list: list,
+                hasNextPage: page < (tvData.total_pages || 1)
+            };
         } catch (e) {
             console.error("search error: " + e);
             return { list: [], hasNextPage: false };
@@ -137,23 +106,31 @@ class DefaultExtension extends MProvider {
         try {
             var parsed = JSON.parse(url);
             var tmdbId = parsed.tmdbId;
-            var type = parsed.type;
+            var imdbId = parsed.imdbId;
+            var type = parsed.type || "tv";
             var isMovie = type === "movie";
-            var endpoint = isMovie ? "/movie/" : "/tv/";
 
-            var data = await this.tmdbGet(endpoint + tmdbId + "?append_to_response=external_ids");
-            var imdbId = data.imdb_id || (data.external_ids && data.external_ids.imdb_id) || null;
+            var data;
+            if (imdbId) {
+                var endpoint = isMovie ? "/movie?imdb_id=" : "/tv?imdb_id=";
+                data = await this.apiGet(this.apiBase + endpoint + imdbId);
+            } else {
+                var tmdbEndpoint = isMovie ? "/movie/" : "/tv/";
+                var tmdbData = await this.tmdbGet(tmdbEndpoint + tmdbId + "?append_to_response=external_ids");
+                imdbId = tmdbData.imdb_id || (tmdbData.external_ids && tmdbData.external_ids.imdb_id) || null;
+                data = { name: tmdbData.title || tmdbData.name, poster: tmdbData.poster_path ? (this.imgBase + tmdbData.poster_path) : "", overview: tmdbData.overview, seasons: tmdbData.seasons, imdb_id: imdbId, tmdb_id: tmdbId };
+            }
 
-            var name = data.title || data.name || "Unknown";
+            var name = data.name || data.title || "Unknown";
             var chapters = [];
 
             if (isMovie) {
                 chapters.push({
                     name: "Movie",
                     url: JSON.stringify({
-                        tmdbId: tmdbId,
-                        type: "movie",
-                        imdbId: imdbId
+                        tmdbId: data.tmdb_id || tmdbId,
+                        imdbId: data.imdb_id || imdbId,
+                        type: "movie"
                     })
                 });
             } else {
@@ -166,9 +143,9 @@ class DefaultExtension extends MProvider {
                         chapters.push({
                             name: "S" + season.season_number + " E" + ep,
                             url: JSON.stringify({
-                                tmdbId: tmdbId,
+                                tmdbId: data.tmdb_id || tmdbId,
+                                imdbId: data.imdb_id || imdbId,
                                 type: "tv",
-                                imdbId: imdbId,
                                 season: season.season_number,
                                 episode: ep
                             })
@@ -180,7 +157,7 @@ class DefaultExtension extends MProvider {
 
             return {
                 name: name,
-                imageUrl: data.poster_path ? ("https://image.tmdb.org/t/p/w500" + data.poster_path) : "",
+                imageUrl: data.poster || "",
                 description: data.overview || "",
                 chapters: chapters
             };
@@ -194,137 +171,31 @@ class DefaultExtension extends MProvider {
         try {
             var parsed = JSON.parse(url);
             var imdbId = parsed.imdbId;
+            var tmdbId = parsed.tmdbId;
             var season = parsed.season;
             var episode = parsed.episode;
             var isMovie = parsed.type === "movie";
 
-            if (!imdbId) {
-                console.warn("No IMDB ID available for this title");
-                return [];
-            }
-
-            var playUrl = this.streamBase + "/play/" + imdbId;
-            var resp = await this.client.get(playUrl, this.getStreamHeaders());
-            var html = resp.body;
-
-            var fileMatch = html.match(/"file":"([^"]+)"/);
-            if (!fileMatch) {
-                console.warn("Could not extract file URL from stream page");
-                return [];
-            }
-
-            var fileUrl = fileMatch[1].replace(/\\\//g, "/");
-            var decodedFileUrl = decodeURIComponent(fileUrl);
-
-            var keyMatch = html.match(/"key":"([^"]+)"/);
-            if (!keyMatch) {
-                console.warn("Could not extract key from stream page");
-                return [];
-            }
-            var key = keyMatch[1];
-
-            var finalUrl = decodedFileUrl.indexOf("https") >= 0
-                ? decodedFileUrl
-                : this.streamHost + decodedFileUrl;
-
-            var headers = {
-                "Referer": "https://himer365ery.com/",
-                "Origin": "https://himer365ery.com",
-                "X-CSRF-TOKEN": key
-            };
-
-            var playerResp = await this.client.get(finalUrl, headers);
-            var playerData = JSON.parse(playerResp.body);
-
-            var videoList = [];
-            var streamHost = this.streamHost;
-
+            var embedUrl;
             if (isMovie) {
-                for (var i = 0; i < playerData.length; i++) {
-                    var src = playerData[i];
-                    if (src.file && src.title) {
-                        var playlistUrl = streamHost + "/playlist/" + src.file.replace(/~/g, "") + ".txt";
-                        try {
-                            var streamResp = await this.client.get(playlistUrl, headers);
-                            videoList.push({
-                                url: streamResp.body,
-                                quality: src.title,
-                                originalUrl: streamResp.body,
-                                subtitles: [],
-                                headers: {
-                                    "Referer": "https://himer365ery.com/",
-                                    "Origin": "https://himer365ery.com"
-                                }
-                            });
-                        } catch (err) {
-                            console.error("Stream fetch error: " + err);
-                        }
-                    }
-                }
+                embedUrl = imdbId
+                    ? (this.embedBase + "/embed/" + imdbId)
+                    : (this.embedBase + "/embed/" + tmdbId);
             } else {
-                if (!season || !episode) {
-                    console.warn("Season/episode required for TV");
-                    return [];
-                }
-
-                var seasonBlock = null;
-                for (var si = 0; si < playerData.length; si++) {
-                    if (playerData[si].id == season) {
-                        seasonBlock = playerData[si];
-                        break;
-                    }
-                }
-                if (!seasonBlock || !Array.isArray(seasonBlock.folder)) {
-                    console.warn("Season block not found for season " + season);
-                    return [];
-                }
-
-                var episodeBlock = null;
-                for (var ei = 0; ei < seasonBlock.folder.length; ei++) {
-                    if (seasonBlock.folder[ei].episode == episode) {
-                        episodeBlock = seasonBlock.folder[ei];
-                        break;
-                    }
-                }
-                if (!episodeBlock || !Array.isArray(episodeBlock.folder)) {
-                    console.warn("Episode block not found for episode " + episode);
-                    return [];
-                }
-
-                for (var vi = 0; vi < episodeBlock.folder.length; vi++) {
-                    var vsrc = episodeBlock.folder[vi];
-                    if (vsrc.file && vsrc.title) {
-                        var strippedUri = decodeURIComponent(vsrc.file.replace(/~/g, ""));
-                        var vPlaylistUrl = strippedUri.indexOf("playlist") >= 0
-                            ? streamHost + "/" + strippedUri
-                            : streamHost + "/playlist/" + strippedUri + ".txt";
-                        try {
-                            var vResp = await this.client.get(vPlaylistUrl, headers);
-                            videoList.push({
-                                url: vResp.body,
-                                quality: vsrc.title,
-                                originalUrl: vResp.body,
-                                subtitles: [],
-                                headers: {
-                                    "Referer": "https://himer365ery.com/",
-                                    "Origin": "https://himer365ery.com"
-                                }
-                            });
-                        } catch (verr) {
-                            console.error("Stream fetch error: " + verr);
-                        }
-                    }
-                }
-
-                // Dub-first: sort so dub entries appear first
-                videoList.sort(function(a, b) {
-                    var aIsDub = a.quality && a.quality.toLowerCase().indexOf("dub") >= 0 ? 0 : 1;
-                    var bIsDub = b.quality && b.quality.toLowerCase().indexOf("dub") >= 0 ? 0 : 1;
-                    return aIsDub - bIsDub;
-                });
+                var id = imdbId || tmdbId;
+                embedUrl = this.embedBase + "/embedtv/" + id + "&s=" + season + "&e=" + episode;
             }
 
-            return videoList;
+            return [{
+                url: embedUrl,
+                quality: "2embed",
+                originalUrl: embedUrl,
+                subtitles: [],
+                headers: {
+                    "Referer": "https://www.2embed.cc/",
+                    "Origin": "https://www.2embed.cc"
+                }
+            }];
         } catch (e) {
             console.error("getVideoList error: " + e);
             return [];
